87602d1641d414b0e11c5914d74a3e384d868356
pyrxi <pyrxin@proton.me>
2023-07-25 09:10:12 -0400

    fixed attribution[6]

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main () {
 
-  std::cout << "we need to learn databases before we do this actually -[5]";	
+  std::cout << "we need to learn databases before we do this actually -[6]";	
 
   return 0;
 }
