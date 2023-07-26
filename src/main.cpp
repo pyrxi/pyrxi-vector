fab6cf9332faad8f00ab276a5b0a2711b1dd77e5
pyrxi <pyrxin@proton.me>
2023-07-26 17:22:56 -0400

    added comments, realizing arrays aren't helpful for this

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 int main () {
 
-  std::ofstream arrayOutput("array.pyrxi");
+  std::ofstream arrayOutput("array.pyrxi"); // Opens file
 
-  std::string pyrxi[6] = {"neoxi", "mirior", "emma", "dot", "alex", "ahkto"};
+  std::string pyrxi[6] = {"neoxi", "mirior", "emma", "dot", "alex", "ahkto"}; // pyrxi[0] through [5] for filler text
 
   for (int i = 0; i < 6; i++) {
     arrayOutput << pyrxi[i] << ",";
-  }
-  
+  } // loop through the array
 
-  return 0;
+  arrayOutput.close(); // close file
+
+  return 0; // quit
 }
