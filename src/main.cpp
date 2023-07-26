e6513546024c0ced90c3be185f9d7d7b766db3a3
pyrxi <pyrxin@proton.me>
2023-07-26 19:04:53 -0400

    implemented vector (that was easy)[0]

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,10 @@ int main () {
 
   std::vector<std::string> pyrxi = {"neoxi", "mirior", "emma", "dot", "alex", "ahkto"}; // pyrxi[0] through [5] for filler text
 
-/*  for (int i = 0; i < 6; i++) {
-    arrayOutput << pyrxi[i] << ",";
-  } // loop through the array
+  for (int i = 0; i < 6; i++) {
+      arrayOutput << pyrxi[i] << ",";
+  } // loop through the vector 
 
   arrayOutput.close(); // close file
-*/
   return 0; // quit
 }
