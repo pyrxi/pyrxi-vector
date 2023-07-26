c2ab6dc60fb13f371875894feead36feec4bd191
pyrxi <pyrxin@proton.me>
2023-07-26 18:07:34 -0400

    it uses a vector and compiles, that's all i'm sure of

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,13 @@ int main () {
 
   std::ofstream arrayOutput("array.pyrxi"); // Opens file
 
-  std::string pyrxi[6] = {"neoxi", "mirior", "emma", "dot", "alex", "ahkto"}; // pyrxi[0] through [5] for filler text
+  std::vector<std::string> pyrxi = {"neoxi", "mirior", "emma", "dot", "alex", "ahkto"}; // pyrxi[0] through [5] for filler text
 
-  for (int i = 0; i < 6; i++) {
+/*  for (int i = 0; i < 6; i++) {
     arrayOutput << pyrxi[i] << ",";
   } // loop through the array
 
   arrayOutput.close(); // close file
-
+*/
   return 0; // quit
 }
