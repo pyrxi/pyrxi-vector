ce3e80bc8f537454bffb43473ca8443642906080
pyrxi <pyrxin@proton.me>
2023-07-27 14:34:03 -0400

    added documentation via comments

--- src/parseFile.cc
+++ src/parseFile.cc
@@ -6,17 +6,19 @@
 #include <sstream>
 
 
-std::vector<std::string> parseFile(std::string string_buffer) {
+std::vector<std::string> parseFile(std::string string_buffer) { // takes the input buffer string
 
-  std::vector<std::string> string_contents;
-  std::stringstream test_string(string_buffer);
+  std::vector<std::string> string_contents;                     // creates vector
+  std::stringstream test_string(string_buffer);                 // sets buffer string as stream
 
 
-  while (std::getline(test_string, string_buffer, ',') ) {
-    string_contents.push_back(string_buffer);
+  while (std::getline(test_string, string_buffer, ',') ) {      // while getline is running
+    string_contents.push_back(string_buffer);                   // write each value to vector
   }
 
-  return string_contents; /*This is bad. Don't return the vector.
-                            Return a pointer to the vector in this function.
-                            Learn to implement that. */
+return string_contents;                                         // returns the vector
+
+                                                                /*Don't return the full vector.
+                                                                Return a pointer to the vector.
+                                                                Learn to implement that. */
 }
